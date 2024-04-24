87ca96773908018531ade89750d43606fa8737b7
Carlos Lerma <carlos@accoladesit.com>
2024-04-24 16:49:33 -0500

    updates to content

--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -65,6 +65,7 @@ function setCookie(name, value, days) {
 	})
 
 
+
 	// INITIALIZATION OF BOOTSTRAP DROPDOWN
 	// =======================================================
 	HSBsDropdown.init()
@@ -89,6 +90,17 @@ function setCookie(name, value, days) {
 	  // =======================================================
 	  new HsNavScroller('.js-nav-scroller')
 
+	  var slidesPerView = new Swiper('.js-swiper-slides-per-view', {
+		slidesPerView: 3,
+		spaceBetween: 30,
+		pagination: {
+		  el: '.js-swiper-slides-per-view-pagination',
+		  clickable: true,
+		},
+		navigation: {
+		  nextEl: '.js-swiper-slides-per-view-button-next',
+		  prevEl: '.js-swiper-slides-per-view-button-prev',
+		},
 
 	  // INITIALIZATION OF LEAFLET
 	  // =======================================================
