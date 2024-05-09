e378e295bfeaf2d0dbd73112bc9ef44b50a85595
Carlos Lerma <carlos@accoladesit.com>
2024-05-09 09:07:46 -0500

    update to content

--- _directory/superior-grill.md
+++ _directory/superior-grill.md
@@ -1,9 +1,33 @@
 ---
 layout: listing
 name: Superior Grill
-title: Superior Grill
+title: Superior Grill, Lafayette LA | Scout Directory
+description: Superior Grill has been rated consistently as one of Louisiana’s best Mexican restaurants since 1983 when our first establishment opened in Shreveport. Our Baton Rouge location opened in 1991 and has become a landmark in fine quality foods with a festive Mexican atmosphere.
+keywords: Superior Grill, Lafayette, LA, Mexican food, sizzling fajitas, mouth-watering enchiladas, cheesy nachos, famous margaritas, Mexican cuisine, Lafayette dining, Superior Grill menu, best fajitas, authentic Mexican, Mexican restaurant, delicious margaritas, cheesy dishes, grilled fajitas, local dining, Tex-Mex, tasty enchiladas, popular restaurant
 image: /assets/img/img_8669.jpg
-short_description: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
-description:
-keywords:
+short_description: Superior Grill has been rated consistently as one of Louisiana’s best Mexican restaurants since 1983 when our first establishment opened in Shreveport. Our Baton Rouge location opened in 1991 and has become a landmark in fine quality foods with a festive Mexican atmosphere.
+location: 2320 Kaliste Saloom Rd, Lafayette, LA 70508
+website: https://lafayette.superiorgrill.com
+type: Restraunt
+services_provided: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
 ---
+ <p>Superior Grill has been rated consistently as one of Louisiana’s best Mexican restaurants since 1983 when our first establishment opened in Shreveport. Our Baton Rouge location opened in 1991 and has become a landmark in fine quality foods with a festive Mexican atmosphere.</p>
+
+<p> We’re famous for our sizzling fajitas and Superior margaritas. Our margaritas are made with freshly squeezed lime juice, and our food is made fresh, from scratch everyday. For added flavor, all of our meats are prepared over a mesquite grill. We are proud of what we make and how we prepare it.
+</p>
+<p>
+ Superior Grill is “the place to be” for happy hour, birthdays, office parties, and large groups. In addition to our festive indoor dining room, we offer a memorable outdoor dining experience on our landscaped patio. We also host several events throughout the year. Find out more here.
+</p>
+<p>
+ Stop in on Saturdays and Sundays between 9am-2pm for our brunch menu.
+</p>
+<p>
+ Looking for entertainment? Join us on our patio every Sunday for live music during our Jazz Brunch.
+</p>
+<p>
+ If you can’t get enough of our delicious food, we offer Fajita Trays and other party trays that are perfect for office luncheons, parties at home, or tailgating.
+</p>
+<p>
+ Make Superior Grill your spot for breakfast, lunch, happy hour or dinner!
+ Superior Grill MidCity BR is one of two locations in Baton Rouge, and one of five Superior Grill locations including Shreveport, New Orleans, and Lafayette (Coming Soon). We are also part of Superior’s Steakhouse and Superior Seafood & Oyster Bar.
+</p>
--- assets/js/theme-custom.js
+++ assets/js/theme-custom.js
@@ -1,195 +1,164 @@
-
-// Email Popup
- function getCookie(name) {
-  const value = '; ' + document.cookie;
-  const parts = value.split('; ' + name + '=');
-  if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-function setCookie(name, value, days) {
-  const d = new Date();
-  d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-  const expires = 'expires=' + d.toUTCString();
-  document.cookie = name + '=' + value + '; ' + expires + '; path=/';
-}
-
-
-// Email Form
-  ;(function (d, s, c) {
-	  var j = d.createElement(s),
-		t = d.getElementsByTagName(s)[0]
-	  j.src = "https://sibforms.com/forms/end-form/elastic-apm-rum.umd.min.js"
-	  j.onload = function () { elasticApm.init(c) }
-	  t.parentNode.insertBefore(j, t)
-  })(document, 'script', { serviceName: 'End Forms', serverUrl: 'https://596808a16dec4fc39413bf34b0a70240.apm.eu-west-1.aws.cloud.es.io:443' , environment : 'production'})
-
-  window.REQUIRED_CODE_ERROR_MESSAGE = 'Please choose a country code';
-  window.LOCALE = 'en';
-							  window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
-
-							  window.REQUIRED_ERROR_MESSAGE = "This field cannot be left blank. ";
-
-  window.GENERIC_INVALID_MESSAGE = "The information provided is invalid. Please review the field format and try again.";
-
-  window.translation = {
-	  common: {
-		  selectedList: '{quantity} list selected',
-		  selectedLists: '{quantity} lists selected'
-	  }
-  };
-
-  var AUTOHIDE = Boolean(0);
-
-  //Site Scripts
-  (function() {
-	// INITIALIZATION OF BOOTSTRAP VALIDATION
-	HSBsValidation.init('.js-validate', {
-	  onSubmit: data => {
-		data.event.preventDefault()
-		alert('Submited')
-	  }
-	})
-
-
-
-	// INITIALIZATION OF BOOTSTRAP DROPDOWN
-	// =======================================================
-	HSBsDropdown.init()
-
-
-	  // INITIALIZATION OF STICKY BLOCKS
-	  // =======================================================
-	  new HSStickyBlock('.js-sticky-block', {
-		targetSelector: document.getElementById('header').classList.contains('navbar-fixed') ? '#header' : null
-	  })
-
-	// INITIALIZATION OF GO TO
-	// =======================================================
-	new HSGoTo('.js-go-to')
-
-
-	// INITIALIZATION OF TEXT ANIMATION (TYPING)
-	// =======================================================
-	HSCore.components.HSTyped.init('.js-typedjs')
-	// INITIALIZATION OF STICKY BLOCKS
-	  // =======================================================
-	  new HSStickyBlock('.js-sticky-block', {
-		targetSelector: document.getElementById('header').classList.contains('navbar-fixed') ? '#header' : null
-	  })
-
-
-	  // INITIALIZATION OF NAV SCROLLER
-	  // =======================================================
-	  new HsNavScroller('.js-nav-scroller')
-
-	  var slidesPerView = new Swiper('.js-swiper-slides-per-view', {
-		slidesPerView: 3,
-		spaceBetween: 30,
-		pagination: {
-		  el: '.js-swiper-slides-per-view-pagination',
-		  clickable: true,
-		},
-		navigation: {
-		  nextEl: '.js-swiper-slides-per-view-button-next',
-		  prevEl: '.js-swiper-slides-per-view-button-prev',
-		},
-  })();
-
-	$(function() {
-	const cookieName = 'modalShown';
-	const cookieValue = getCookie(cookieName);
-
-	if (!cookieValue) {
-	  setTimeout(function() {
-		$('#exampleModalCenter').modal('show');
-		$('#exampleModalCenter').on('hidden.bs.modal', function() {
-		  const daysIn9999Years = 9999 * 365;
-		  setCookie(cookieName, 'true', daysIn9999Years);
-		});
-	  }, 5000);
+// Site Scripts
+;(function() {
+  // INITIALIZATION OF BOOTSTRAP VALIDATION
+  HSBsValidation.init('.js-validate', {
+	onSubmit: data => {
+	  data.event.preventDefault();
+	  alert('Submited');
 	}
-	// =======================================================
-	  AOS.init({
-		duration: 650,
-		once: true
-	  });
+  });
 
+  // INITIALIZATION OF BOOTSTRAP DROPDOWN
+  HSBsDropdown.init();
 
-	  // INITIALIZATION OF SWIPER
-	  // =======================================================
-	  let activeIndex = 0
-	  var sliderThumbs = new Swiper('.js-swiper-thumbs', {
-		slidesPerView: 1,
-		autoplay: false,
-		watchSlidesVisibility: true,
-		watchSlidesProgress: true,
-		followFinger: false,
-		loop: true,
-		on: {
-		  'slideChangeTransitionEnd': function (event) {
-			if (sliderMain === undefined) return
-			sliderMain.slideTo(event.activeIndex)
+  // INITIALIZATION OF STICKY BLOCKS
+  new HSStickyBlock('.js-sticky-block', {
+	targetSelector: document.getElementById('header').classList.contains('navbar-fixed') ? '#header' : null
+  });
+
+  // INITIALIZATION OF GO TO
+  new HSGoTo('.js-go-to');
+
+  // INITIALIZATION OF TEXT ANIMATION (TYPING)
+  HSCore.components.HSTyped.init('.js-typedjs');
+
+  // INITIALIZATION OF NAV SCROLLER
+  new HsNavScroller('.js-nav-scroller');
+
+  var slidesPerView = new Swiper('.js-swiper-slides-per-view', {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	pagination: {
+	  el: '.js-swiper-slides-per-view-pagination',
+	  clickable: true
+	},
+	navigation: {
+	  nextEl: '.js-swiper-slides-per-view-button-next',
+	  prevEl: '.js-swiper-slides-per-view-button-prev'
+	}
+  });
+
+  $(function() {
+	// Check if the element that uniquely identifies the page is present
+	if (document.querySelector('#exampleModalCenter')) {
+	  // Define the getCookie function
+	  function getCookie(name) {
+		const value = '; ' + document.cookie;
+		const parts = value.split('; ' + name + '=');
+		if (parts.length === 2) return parts.pop().split(';').shift();
+		return null; // Return null if the cookie isn't found
+	  }
+
+	  // Define the setCookie function
+	  function setCookie(name, value, days) {
+		const d = new Date();
+		d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+		const expires = 'expires=' + d.toUTCString();
+		document.cookie = name + '=' + value + '; ' + expires + '; path=/';
+	  }
+
+	  // Ensure that the modal logic only runs on relevant pages by checking if an identifying element exists
+	  if (document.querySelector('#exampleModalCenter')) {
+		$(function() {
+		  const cookieName = 'modalShown'; // Declare cookieName within scope
+		  const cookieValue = getCookie(cookieName); // Get the value of the cookie
+
+		  // Show the modal if the cookie isn't set (meaning the modal hasn't been shown before)
+		  if (!cookieValue) {
+			setTimeout(function() {
+			  $('#exampleModalCenter').modal('show');
+			  $('#exampleModalCenter').on('hidden.bs.modal', function() {
+				// Set a cookie to remember that the modal has been shown
+				const daysIn9999Years = 9999 * 365;
+				setCookie(cookieName, 'true', daysIn9999Years);
+			  });
+			}, 5000); // Delay before showing the modal
 		  }
+		});
+	  }
+
+	}
+
+
+	// INITIALIZATION OF AOS
+	AOS.init({
+	  duration: 650,
+	  once: true
+	});
+
+	// INITIALIZATION OF SWIPER THUMBS
+	let sliderThumbs = new Swiper('.js-swiper-thumbs', {
+	  slidesPerView: 1,
+	  autoplay: false,
+	  watchSlidesVisibility: true,
+	  watchSlidesProgress: true,
+	  followFinger: false,
+	  loop: true,
+	  on: {
+		'slideChangeTransitionEnd': function(event) {
+		  if (sliderMain === undefined) return;
+		  sliderMain.slideTo(event.activeIndex);
 		}
-	  });
+	  }
+	});
 
-	  var sliderMain = new Swiper('.js-swiper-main', {
-		effect: 'fade',
-		autoplay: false,
-		disableOnInteraction: true,
-		loop: true,
-		navigation: {
-		  nextEl: '.js-swiper-main-button-next',
-		  prevEl: '.js-swiper-main-button-prev',
-		},
-		thumbs: {
-		  swiper: sliderThumbs
-		},
-		on: {
-		  'slideChangeTransitionEnd': function (event) {
-			if (sliderThumbs === undefined) return
-			sliderThumbs.slideTo(event.activeIndex)
-		  }
+	let sliderMain = new Swiper('.js-swiper-main', {
+	  effect: 'fade',
+	  autoplay: false,
+	  disableOnInteraction: true,
+	  loop: true,
+	  navigation: {
+		nextEl: '.js-swiper-main-button-next',
+		prevEl: '.js-swiper-main-button-prev'
+	  },
+	  thumbs: {
+		swiper: sliderThumbs
+	  },
+	  on: {
+		'slideChangeTransitionEnd': function(event) {
+		  if (sliderThumbs === undefined) return;
+		  sliderThumbs.slideTo(event.activeIndex);
 		}
-	  })
+	  }
+	});
 
+	// Clients Swiper
+	let swiperClients = new Swiper('.js-swiper-clients', {
+	  slidesPerView: 2,
+	  breakpoints: {
+		380: {
+		  slidesPerView: 3,
+		  spaceBetween: 15
+		},
+		768: {
+		  slidesPerView: 4,
+		  spaceBetween: 15
+		},
+		1024: {
+		  slidesPerView: 5,
+		  spaceBetween: 15
+		}
+	  }
+	});
 
-	   // Clients
-	   var swiper = new Swiper('.js-swiper-clients',{
-		 slidesPerView: 2,
-		 breakpoints: {
-		   380: {
-			 slidesPerView: 3,
-			 spaceBetween: 15,
-		   },
-		   768: {
-			 slidesPerView: 4,
-			 spaceBetween: 15,
-		   },
-		   1024: {
-			 slidesPerView: 5,
-			 spaceBetween: 15,
-		   },
-		 },
-	   });
-
-	   // Card Grid
-	   var swiper = new Swiper('.js-swiper-card-blocks',{
-		 slidesPerView: 1,
-		 pagination: {
-		   el: '.js-swiper-card-blocks-pagination',
-		   dynamicBullets: true,
-		   clickable: true,
-		 },
-		 breakpoints: {
-		   620: {
-			 slidesPerView: 2,
-			 spaceBetween: 15,
-		   },
-		   1024: {
-			 slidesPerView: 3,
-			 spaceBetween: 15,
-		   },
-		 },
-	   });
-  })();
+	// Card Grid Swiper
+	let swiperCardBlocks = new Swiper('.js-swiper-card-blocks', {
+	  slidesPerView: 1,
+	  pagination: {
+		el: '.js-swiper-card-blocks-pagination',
+		dynamicBullets: true,
+		clickable: true
+	  },
+	  breakpoints: {
+		620: {
+		  slidesPerView: 2,
+		  spaceBetween: 15
+		},
+		1024: {
+		  slidesPerView: 3,
+		  spaceBetween: 15
+		}
+	  }
+	});
+  });
+})();
